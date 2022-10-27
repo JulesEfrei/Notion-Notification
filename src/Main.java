487aee31711a5806052bdc2ac672b734365ef53c
JulesEfrei <jules.bruzeau@efrei.net>
2022-10-27 10:02:01 +0200

    Fix : CmdExecution

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import CMD.CmdExecution;
+import Env.EnvReader;
 
 public class Main {
     public static void main(String[] args) {
 
         CmdExecution terminal = new CmdExecution();
+        terminal.addCommand("pwd");
+        terminal.execute();
 
-        terminal.getCommand();
-        terminal.addCommand("osascript diaplay notification 'Test'");
-        terminal.execute(false);
+        EnvReader test = new EnvReader();
+        System.out.printf(test.getNotion_token() + "\n");
+        System.out.printf(test.getDatabase_id() + "\n");
+        System.out.printf(test.getDatabase_url());
 
     }
 }
